38f77edbf7d0e65c221c03f0a23e5289071df9ad
Matt Spooner <matt.spooner10@gmail.com>
2014-10-10 18:27:11 -0400

    Fixed some bugs
    Fixed labels being unreadable. The graph was offset wrong.  Make the
    scale able to be a double.

--- src/ProjectileComponent.java
+++ src/ProjectileComponent.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Polygon;
 
@@ -11,26 +12,31 @@ public class ProjectileComponent extends JComponent{
 	//Physics Constants
 	private static final double X_POSITION_INITIAL = 0; //m
 	private static final double Y_POSITION_INITIAL = 0; //m
-	private static final double VELOCITY_INITIAL = 5.0; //m/s
-	private static final double LAUNCH_ANGLE = 45.0; //degrees
-	private static final double X_ACCELERATION = 0.0; //m/s^2
-	private static final double Y_ACCELERATION = -9.81;//m/s^2
+	private static final double VELOCITY_INITIAL = 10.0; //m/s
+	private static final double LAUNCH_ANGLE = 50.0; //degrees
+	private static final double X_ACCELERATION = -0.6; //m/s^2
+	private static final double Y_ACCELERATION = -2.81;//m/s^2
+	
+	private static final double X_VELOCITY_INITIAL = VELOCITY_INITIAL * Math.cos(Math.toRadians(LAUNCH_ANGLE));; //m/s
+	private static final double Y_VELOCITY_INITIAL = VELOCITY_INITIAL * Math.sin(Math.toRadians(LAUNCH_ANGLE)); //m/s
 	
 	//Graph Constants
 	private static final int DEFAULT_SCALE = 2; //m
 	private static final int DEFAULT_OFFSET = 25; //px
-	private static final int TICK_LENGTH = 5; //px
-	private static final int TICK_SPACING = 15; //px
-	private static final int ARROW_LENGTH = 15; //px
-	private static final int ARROW_WIDTH = 8; //px
-	private static final int NUM_POINTS=100;
-	private static final int X_LABEL_DISTANCE = 5; //px
-	private static final int Y_LABEL_DISTANCE = 15; //px
-	private static final int LABEL_PLACES = 3;
+	private static final int TICK_LENGTH = 10; //px
+	private static final int TICK_SPACING = 45; //px
+	private static final int ARROW_LENGTH = 25; //px
+	private static final int ARROW_WIDTH = 10; //px
+	private static final int NUM_POINTS=30;
+	private static final int X_LABEL_DISTANCE = 20; //px
+	private static final int Y_LABEL_DISTANCE = 30; //px
+	private static final int MAX_SCALE_DECIMAL_PLACES = 4;
+	private static final int X_LABEL_OFFSET = 15; //px
+	private static final Font GRAPH_FONT = new Font("monospaced", Font.PLAIN, 11);
 	
 	//Members
-	private int xScale;
-	private int yScale;
+	private double xScale;
+	private double yScale;
 	private int xOffset;
 	private int yOffset;
 	
@@ -38,53 +44,56 @@ public class ProjectileComponent extends JComponent{
 	protected ProjectileComponent(){
 		this(DEFAULT_SCALE, DEFAULT_SCALE);
 	}
-	protected ProjectileComponent(int xScale, int yScale, int xOffset, int yOffset){
+	protected ProjectileComponent(double xScale, double yScale, int xOffset, int yOffset){
 		this.xScale = xScale;
 		this.yScale = yScale;
 		this.xOffset = xOffset;
 		this.yOffset = yOffset;
 	}
-	protected ProjectileComponent(int xScale, int yScale){
+	protected ProjectileComponent(double xScale, double yScale){
 		this(xScale, yScale, DEFAULT_OFFSET, DEFAULT_OFFSET);
+	}
+	protected ProjectileComponent(int xOffset, int yOffset){
+		this(DEFAULT_SCALE, DEFAULT_SCALE, xOffset, yOffset);
 	}
 	//Methods
 	@Override
 	protected void paintComponent(Graphics g) {
+		g.setFont(GRAPH_FONT);
 		drawAxis(g);
 		plot(g);
 	}
 	private void drawAxis(Graphics g){
 		final int WIDTH = getWidth();
 		final int HEIGHT = getHeight();
-		//width and height of the graphable plane
-		final int P_WIDTH = WIDTH - xOffset - ARROW_LENGTH;
-		final int P_HEIGHT = HEIGHT - yOffset - ARROW_LENGTH;
+		//width and height of the offset plane (includes arrow length)
+		final int OFF_WIDTH = WIDTH - ARROW_LENGTH;
+		final int OFF_HEIGHT = HEIGHT - yOffset;
 		//draw the x axis
-		g.drawLine(xOffset, P_HEIGHT, WIDTH, P_HEIGHT);
+		g.drawLine(xOffset, OFF_HEIGHT, OFF_WIDTH, OFF_HEIGHT);
 		//draw the y axis
-		g.drawLine(xOffset, ARROW_LENGTH, xOffset, P_HEIGHT);
+		g.drawLine(xOffset, ARROW_LENGTH, xOffset, OFF_HEIGHT);
 		//draw the x tick marks
-		for(int x = xOffset; x <= P_WIDTH; x += TICK_SPACING){
-			g.drawLine(x, P_HEIGHT, x, P_HEIGHT + TICK_LENGTH);
+		for(int x = xOffset; x < OFF_WIDTH; x += TICK_SPACING){
+			g.drawLine(x, OFF_HEIGHT, x, OFF_HEIGHT + TICK_LENGTH);
 		}
 		//draw the y tick marks
-		for(int y = P_HEIGHT; y >= ARROW_LENGTH; y -= TICK_SPACING){
-			g.drawLine(yOffset, y, yOffset - TICK_LENGTH, y);
+		for(int y = OFF_HEIGHT; y > ARROW_LENGTH; y -= TICK_SPACING){
+			g.drawLine(xOffset, y, xOffset - TICK_LENGTH, y);
 		}
 		//draw the x arrow
 		drawArrow(g, false);
 		//draw the y arrow
 		drawArrow(g, true);
 		//draw the x labels
-		for(int x = xOffset; x <= P_WIDTH; x += TICK_SPACING){
+		for(int x = xOffset; x < OFF_WIDTH; x += TICK_SPACING){
 			int scaleMark = (x - xOffset) / TICK_SPACING;
 			String label = getLabel(scaleMark * xScale);
-			g.drawString(label, x, HEIGHT - yOffset + X_LABEL_DISTANCE);
+			g.drawString(label, x - X_LABEL_OFFSET, OFF_HEIGHT + X_LABEL_DISTANCE);
 		}
 		//draw the y labels
-		//TODO
-		for(int y = P_HEIGHT; y >= ARROW_LENGTH; y -= TICK_SPACING){
-			int scaleMark = (P_HEIGHT - y) / TICK_SPACING;
+		for(int y = OFF_HEIGHT; y > ARROW_LENGTH; y -= TICK_SPACING){
+			int scaleMark = (OFF_HEIGHT - y) / TICK_SPACING;
 			String label = getLabel(scaleMark * yScale);
 			g.drawString(label, xOffset - Y_LABEL_DISTANCE, y);
 		}
@@ -119,35 +128,31 @@ public class ProjectileComponent extends JComponent{
 	}
 	private void plot(Graphics g){
 		//TODO
+		//TODO only do calculation once, not everytime paint is called
 		//calculate the time for the projectile to hit the ground
-		double timeForLanding = (Math.sqrt(-Y_POSITION_INITIAL * 2 * Y_ACCELERATION + Math.pow(calculateYVelocity(), 2)) - calculateYVelocity()) / Y_ACCELERATION; //TODO
-		System.out.println(timeForLanding);
+		
 		//divide that by the NUM_POINTS to get the time between each point
+		
 		//Iterate through each time and find the x and y position at it
+		
 		//Draw and arc or line through multiple points to draw the graph
-	}
-	private double calculateXPosition(double time){
 		//TODO
-		return 0.0d;
 	}
-	private double calculateYPosition(double time){
-		//TODO
-		return 0.0d;
-	}
-	private String getLabel(int scale){
-		//TODO
-		//adds spaces to line everything up
-		String label = Integer.toString(scale);
-		while(label.length() < LABEL_PLACES){
-			label = " " + label;
+	
+	private String getLabel(double scale){
+		//adds spaces to line everything up and reformats the scale
+		String label = String.format("%." + MAX_SCALE_DECIMAL_PLACES + "f", scale);
+		//while it has insignificant 0s or the . and has a . and is not a single 0
+		while((label.endsWith("0") || label.endsWith(".")) && label.contains(".") && label.length() > 1){
+			//chop off last digit
+			label = label.substring(0, label.length()-1);
+		}
+		//if starts with a 0 and not a single number,
+		if(label.startsWith("0") && label.length() > 1){
+			//chop off the leading 0
+			label = label.substring(1);
 		}
 		return label;
 	}
-	//to do this (just plot points), use range equation, get final distance, divide by number of points, and then add interval from that to initial, then repeat for number of points. 
-	private double calculateXVelocity(){
-		return VELOCITY_INITIAL * Math.cos(Math.toRadians(LAUNCH_ANGLE));
-	}
-	private double calculateYVelocity(){
-		return VELOCITY_INITIAL * Math.sin(Math.toRadians(LAUNCH_ANGLE));
-	}
+	//TODO this (just plot points), use range equation, get final distance, divide by number of points, and then add interval from that to initial, then repeat for number of points. 
 }
--- src/ProjectileRunner.java
+++ src/ProjectileRunner.java
@@ -7,17 +7,15 @@ public class ProjectileRunner {
 	/*
 	 * TODO:
 	 * What needs to be done:
-	 * draw the scale under each tick mark
 	 * draw the arrowheads
 	 * draw the actual graph
-	 * write the physics equations into methods
-	 * labels overlap, fix this
+	 * optimise drawing to not calculate all the math each time repaint is called
 	 */
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Projectile Physics");
-		frame.setSize(300, 200);
+		frame.setSize(500, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ProjectileComponent pc = new ProjectileComponent();
+		ProjectileComponent pc = new ProjectileComponent(10, 10, 55, 55);
 		frame.add(pc);
 		frame.setVisible(true);
 	}
